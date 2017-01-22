3c484f9470e5da6f33cfb3ef14c2be080d9400c5
Sathish <Sathish.Sankaran@gmail.com>
2017-01-21 22:19:15 -0600

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+rm(list=ls())
+cat("\014")
 
-## Write a short comment describing this function
+## Cache matrix inversion
+
+## makeCacheMatrix function returns a list of functions to:
+## 1. Set matrix
+## 2. Get matrix
+## 3. Set inverse of matrix
+## 4. Get inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Check if cache exists, else compute and return matrix inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
+
+m <- matrix(rnorm(1000000),nrow=1000,ncol=1000)
+mv <- makeCacheMatrix(m)
+mv$set(m)
+
+ptm <- proc.time()
+cacheSolve(mv)
+print(proc.time()-ptm)
+
+ptm <- proc.time()
+cacheSolve(mv)
+print(proc.time()-ptm)
